6f63fa300174e3280d990ee81f0c4dabb5d4edab
TheGabmeister <thegabmeister@protonmail.com>
2025-06-25 10:08:56 +0800

    Fix LevelData table

--- CrashBandicoot/Source/CrashBandicoot/LevelData.h
+++ CrashBandicoot/Source/CrashBandicoot/LevelData.h
@@ -3,33 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Engine/DataAsset.h"
 #include "LevelData.generated.h"
 
 USTRUCT()
-struct FMyAssetInfo {
-	GENERATED_BODY()
-				 
-	UPROPERTY(EditAnywhere)
-	FString AssetName;
-				 
-	UPROPERTY(EditAnywhere)
-	UTexture2D* AssetThumbnail;
-				 
-	UPROPERTY(EditAnywhere)
-	UStaticMesh* AssetStaticMesh; 
-};
-
-UCLASS()
-class CRASHBANDICOOT_API ULevelData : public UPrimaryDataAsset
+struct FLevelData : public FTableRowBase
 {
 	GENERATED_BODY()
-
-//	UPROPERTY(EditAnywhere)
-//	TArray<FMyAssetInfo> AssetItems;
-
-	UPROPERTY(EditAnywhere, meta=(EditFixedOrder))
-	TMap<TSoftObjectPtr<UWorld>, USoundBase*> LevelData;
-
-	
+				 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftObjectPtr<UWorld> Level;
+				 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	USoundBase* Music;
 };
