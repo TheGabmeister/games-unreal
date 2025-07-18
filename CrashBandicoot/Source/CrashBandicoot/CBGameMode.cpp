de1bf806ce14c40aefbdc81d5645e8c4a027348b
TheGabmeister <thegabmeister@protonmail.com>
2025-07-18 13:49:13 +0800

    Update CBGameMode.cpp

--- CrashBandicoot/Source/CrashBandicoot/CBGameMode.cpp
+++ CrashBandicoot/Source/CrashBandicoot/CBGameMode.cpp
@@ -16,10 +16,8 @@ ACBGameMode::ACBGameMode()
 void ACBGameMode::BeginPlay()
 {
 	Super::BeginPlay();
-    
-	// Get the Gameplay Message Subsystem
+	
 	UGameplayMessageSubsystem& MessageSubsystem = UGameplayMessageSubsystem::Get(this);
-    
 	UAsyncAction_ListenForGameplayMessage* Listener = UAsyncAction_ListenForGameplayMessage::ListenForGameplayMessages(
 		this,
 		Events_OnPickedUp_WumpaFruit,
@@ -31,16 +29,13 @@ void ACBGameMode::BeginPlay()
 		Listener->OnMessageReceived.AddDynamic(this, &ThisClass::OnGameplayMessageReceived);
 		Listener->Activate();
 	}
-
-    
-	UE_LOG(LogTemp, Log, TEXT("CBGameMode: Registered listener for WumpaFruit pickup events"));
 }
 
 void ACBGameMode::OnGameplayMessageReceived(UAsyncAction_ListenForGameplayMessage* Proxy, FGameplayTag ActualChannel)
 {
-	//FGameplayMessageInt Payload;
-	//if (Proxy->GetPayload(Payload))
-	//{
-	//	UE_LOG(LogTemp, Log, TEXT("CBGameMode: Payload WumpaFruit pickup events: %d"), Payload.Value);
-	//}
+	FGameplayMessageInt Payload;
+	if (Proxy->GetPayload(Payload.Value))
+	{
+		UE_LOG(LogTemp, Log, TEXT("CBGameMode: Payload WumpaFruit pickup events: %d"), Payload.Value);
+	}
 }
